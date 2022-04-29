d82a9aa83131ac6b71707350eb3f4940eb82f74e
Sofiane Bouaziz <is_bouaziz@esi.dz>
2022-04-29 17:53:18 +0100

    xGEMM_Halide V1.1

--- script.sh
+++ script.sh
@@ -2,25 +2,25 @@ read -p "Please provide the absolute or a relative path to the halide folder tha
 echo "Deleting ./data/result.txt if it exists"
 sudo rm -f ./result/result.txt
 
-echo "**********************************"
+echo "***********************************"
 
-echo "Compiling g./code/gemm_halide.cpp..."
+echo "Compiling ./code/gemm_halide.cpp..."
 g++ ./code/gemm_halide.cpp -g -I ${path}/include -L ${path}/src -I ${path}/tools  -lHalide -o ./bin/gemm_halide -std=c++17
 
-echo "**********************************"
+echo "***********************************"
 
 echo "Compiling ./code/gemm_c.c..."
 gcc ./code/gemm_c.c -o ./bin/gemm_c
 
-echo "**********************************"
+echo "***********************************"
 
-echo "Executing ./bin/gemm_halide"
+echo "Executing ./bin/gemm_halide..."
 LD_LIBRARY_PATH=${path}/src  ./bin/gemm_halide
 
 
-echo "**********************************"
+echo "***********************************"
 
-echo "Executing./bin/gemm_c"
+echo "Executing./bin/gemm_c..."
 ./bin/gemm_c
 
-echo "**********************************"
+echo "***********************************"
